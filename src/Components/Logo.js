f79a94637f6724c169de7594ccaf998490d9d100
Kevin Horan <khoran@galcom.org>
2022-11-16 13:39:58 -0500

    added noreferrer to link with target

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -5,7 +5,7 @@ function Logo(props) {
   const img = <img src={props.logo} alt="logo" id="logo" />;
   if(props.stationWebsite != null){
 
-    return <a href={props.stationWebsite} target="_blank" rel="noopener">{img}</a>;
+    return <a href={props.stationWebsite} target="_blank" rel="noopener noreferrer">{img}</a>;
   }else
     return  img;
 }
